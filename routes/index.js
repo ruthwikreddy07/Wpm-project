32974b15886a6c1d2c887cb16daf420a534ce2ca
Donthireddy Ruthwik Reddy <179065000+ruthwikreddy07@users.noreply.github.com>
2025-08-20 12:23:09 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Ruthwik reddy' });
+  res.render('index', { title: 'EventHub' });
 });
 
 module.exports = router;
