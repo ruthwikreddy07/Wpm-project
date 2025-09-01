e17258028fccfaf9aa9f73d741071116f35df533
ruthwikreddy07 <179065000+ruthwikreddy07@users.noreply.github.com>
2025-09-01 18:44:25 +0530

    updates

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,17 +1,22 @@
 /* GET 'home' page */
 const homelist = (req, res) => {
- res.render('index', { title: 'Home' });
+ res.render('locations-list', { title: 'Home' });
 };
 /* GET 'Location info' page */
 const locationInfo = (req, res) => {
- res.render('index', { title: 'Location info' });
+ res.render('location-info', { title: 'Location info' });
 };
 /* GET 'Add review' page */
 const addReview = (req, res) => {
- res.render('index', { title: 'Add review' });
+ res.render('location-review-form', { title: 'Add review' });
+};
+/* GET 'about' page */
+const about = (req, res) => {
+  res.render('generic-text', { title: 'About' });
 };
 module.exports = {
  homelist,
  locationInfo,
- addReview
+ addReview,
+ about
 };
